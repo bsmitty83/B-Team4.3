22193f2c5dde43b6aeaa4d80c4d8330ce8bece06
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-08-11 13:24:44 -0400

    Revert " gpu: Glitch free GPU frequency changing (MikeC84)"
    This reverts commit 5325c1c72121e2f8f0b5324f4df70b2ad3a0fff7.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 9 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#10 PREEMPT Sun Aug 11 13:00:43 EDT 2013"
+#define UTS_VERSION "#9 PREEMPT Sun Aug 11 12:46:54 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.8.2 20130624 (prerelease) (crosstool-NG hg+default-81ead21f86c2 - Linaro GCC 2013.07) "
