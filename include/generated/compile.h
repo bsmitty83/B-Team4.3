0e7f262d4bd39c1d1810afad4f94bf20d4d94a2c
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-09-01 22:01:23 -0400

    patch up to linux 3.0.96
    Conflicts:
    	Makefile
    	Module.symvers
    	include/config/auto.conf
    	include/config/auto.conf.cmd
    	include/config/tristate.conf
    	include/generated/autoconf.h
    	include/generated/compile.h
    	scripts/basic/.fixdep.cmd
    	scripts/kconfig/.conf.o.cmd
    	scripts/kconfig/.mconf.cmd
    	scripts/kconfig/.mconf.o.cmd
    	scripts/kconfig/.zconf.tab.o.cmd
    	scripts/kconfig/lxdialog/.checklist.o.cmd
    	scripts/kconfig/lxdialog/.inputbox.o.cmd
    	scripts/kconfig/lxdialog/.menubox.o.cmd
    	scripts/kconfig/lxdialog/.textbox.o.cmd
    	scripts/kconfig/lxdialog/.util.o.cmd
    	scripts/kconfig/lxdialog/.yesno.o.cmd
    	scripts/kconfig/zconf.tab.o

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 17 */
+/* This file is auto generated, version 20 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#17 PREEMPT Sat Sep 21 10:10:00 EDT 2013"
+#define UTS_VERSION "#20 PREEMPT Sat Sep 21 14:19:58 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
